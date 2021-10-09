22f4e7fad7f687cfae7883431e0c2df22b1de215
Eyez <lmt18072747669@gmail.com>
2021-10-09 22:39:33 +0900

    updated utils with singletons and projectUtils static classes

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -9,3 +9,22 @@ static class ShaderID
     public static readonly int DepthRange = Shader.PropertyToID("_DepthRange");
     public static readonly int AspectFix = Shader.PropertyToID("_AspectFix");
 }
+
+static class Singletons
+{
+    static LidarDataProcessor _lidarDataProcessor;
+    public static LidarDataProcessor LidarDataProcessor
+        => _lidarDataProcessor != null ? _lidarDataProcessor :
+        (_lidarDataProcessor = Object.FindObjectOfType<LidarDataProcessor>());
+        
+        
+}
+
+static class ProjectionUtil
+{
+    public static Vector4 GetVector(in Matrix4x4 m)
+      => new Vector4(m[0, 2], m[1, 2], m[0, 0], m[1, 1]);
+
+    public static Vector4 ProjectionVector
+      => GetVector(Singletons.LidarDataProcessor.lidarData.projectionMatrix);
+}
